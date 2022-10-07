4e688d9fe28ce8adbb56ab75cefc0567032e5cb9
ingus26 <ingus26@gmail.com>
2022-10-07 09:14:03 +0900

    feat : 자동차 경주 기능 추가

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -3,6 +3,7 @@ package racingcar;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class RacingCarGame {
@@ -22,6 +23,7 @@ public class RacingCarGame {
         setCars(player);
         player.setRaceCount();
         racing(player.getRaceCount());
+        racingResult();
     }
 
     private void setCars(Player player) {
@@ -57,6 +59,33 @@ public class RacingCarGame {
     private void racingRoundResult() {
         cars.forEach(car -> System.out.printf("%s : %s\n", car.name, car.getRaceStatus()));
         System.out.println(" ");
+    }
+
+    private void racingResult() {
+        int max = getMaxMoveCount();
+
+        cars.removeIf(car -> car.getMoveCount() != max);
+
+        getWinner();
+    }
+
+    private int getMaxMoveCount() {
+        List<Integer> moveCounts = new ArrayList<>();
+
+        cars.forEach(car -> moveCounts.add(car.getMoveCount()));
+
+        return Collections.max(moveCounts);
+    }
+
+    private void getWinner() {
+        System.out.println(Message.WIN.getMessage());
+        StringBuilder winners = new StringBuilder();
+
+        for (Car car : cars) {
+            winners.append(car.getName()).append(Config.PREFIX);
+        }
+
+        System.out.println(winners.substring(0, winners.length() - 1));
     }
 
     private void sizeValidation() {
